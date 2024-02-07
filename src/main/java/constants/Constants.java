86cfc74059c0ea4761a760e0af9547e11e5f18ec
leisanbv <leisan.batdalova@gmail.com>
2024-02-07 22:29:18 +0300

    Small improvements

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -5,6 +5,6 @@ public interface Constants {
     // localhost
     //String URL_TO_DB = "jdbc:mysql://localhost:3306/students40?user=root&password=sqlnwdbfpt&useUnicode=true&serverTimezone=UTC";
 
-    // remote
+    // remote connection
     String URL_TO_DB = "jdbc:mysql://localhost:3306/stud40?user=root19&password=root19011994&useUnicode=true&serverTimezone=UTC";
 }
